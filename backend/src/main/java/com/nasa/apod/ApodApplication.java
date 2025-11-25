cb420c497beea03bdeafe26adcc28ea972f8e2a1
chahats2002@gmail.com <chahats2002@gmail.com>
2025-11-25 15:50:13 +0530

    Initialize Spring Boot project structure

---
+++ backend/src/main/java/com/nasa/apod/ApodApplication.java
@@ -0,0 +1,12 @@
+package com.nasa.apod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApodApplication.class, args);
+	}
+}
